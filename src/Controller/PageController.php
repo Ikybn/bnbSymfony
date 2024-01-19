af9f16dead3407e304c6b5b1036b108e4aad9d21
Ikrame <ikr.projet@gmail.com>
2024-01-19 11:00:04 +0100

    modification controller et vue

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -9,18 +9,45 @@ use Symfony\Component\Routing\Annotation\Route;
 class PageController extends AbstractController
 {
     #[Route('/', name: 'paris')]
-    public function index(): Response
+    public function paris(): Response
     {
-        return $this->render('page/index.html.twig', [
-            
+        return $this->render('page/city.html.twig', [
+            'title' => 'Paris',
+            'background' => 'paris',
         ]);
     }
-
-    #[Route('/email', name: 'email')]
-    public function email(): Response
+    #[Route('/', name: 'lasvegas')]
+    public function lasvegas(): Response
     {
-        return $this->render('registration/confirmation_email.html.twig', [
-            'signedUrl' => 'https://example.com/signed-url',
+        return $this->render('page/city.html.twig', [
+            'title' => 'Las Vegas',
+            'background' => 'Las Vegas',
+        ]);
+    }
+    #[Route('/', name: 'kyoto')]
+    public function kyoto(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Kyoto',
+            'subtitle' => '京都',
+            'background' => 'Kyoto',
+        ]);
+    }
+    #[Route('/', name: 'sydney')]
+    public function sydney(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Sydney',
+            'background' => 'Sydney',
+        ]);
+    }
+    #[Route('/', name: 'hongkong')]
+    public function hongkong(): Response
+    {
+        return $this->render('page/city.html.twig', [
+            'title' => 'Hong Kong',
+            'subtitle' => '香港',
+            'background' => 'Hong Kong',
         ]);
     }
 }
